32a60c9bcdfe0591cff6ebd98ac581e59cb7c7d4
Omer C <639682+omercnet@users.noreply.github.com>
2025-06-19 22:08:12 +0300

    Add .zshrc.d/zoxide.zsh Update .p10k.zsh Update .zshenv Update .zshrc Change attributes of .zsh_plugins.txt

--- private_dot_p10k.zsh
+++ private_dot_p10k.zsh
@@ -53,13 +53,13 @@ fi
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
       virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-      anaconda                # conda environment (https://conda.io/)
+      # anaconda                # conda environment (https://conda.io/)
       pyenv                   # python environment (https://github.com/pyenv/pyenv)
-      nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
+      # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
       nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
-      nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
+      # nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
       # node_version          # node.js version
-      # go_version            # go version (https://golang.org)
+      go_version            # go version (https://golang.org)
       # rust_version          # rustc version (https://www.rust-lang.org)
       rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
       rvm                     # ruby version from rvm (https://rvm.io)
@@ -71,8 +71,8 @@ fi
       # =========================[ Line #2 ]=========================
       newline
       # public_ip             # public IP address
-      # battery               # internal battery
-      # time                  # current time
+      battery               # internal battery
+      time                  # current time
       # example               # example user-defined segment (see prompt_example function below)
   )
 
